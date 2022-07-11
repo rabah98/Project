9c097480a0a1bb562bcbfac66e1dc1f3f1410cf7
Alex <friedmanalexg@gmail.com>
2022-07-11 16:54:45 -0400

    icons now render with weather in cards

--- index.js
+++ index.js
@@ -30,8 +30,27 @@ function funcToRenderDayCards(daysObject) {
       console.log(day)
       const liDate = document.createElement('li')
       const liWeather = document.createElement('li')
+      const weatherIcon = document.createElement( 'img' )
       const dayDate = day.date 
-      const dayWeather = day.weather
+      let dayWeather = day.weather
+      if (dayWeather === 'clear') {
+         weatherIcon.src = `assets/weathericons/clear.png` ;
+       } else if (dayWeather === 'pcloudy') {
+         weatherIcon.src = `assets/weathericons/pcloudy.png` ;
+       } else if (dayWeather === 'cloudy') {
+         weatherIcon.src = `assets/weathericons/cloudy.png` ;
+       } else if (dayWeather === 'rain' || 'lightrain') {
+         weatherIcon.src = `assets/weathericons/rain.png` ;   
+       } else if (dayWeather === 'snow' || 'lightsnow') {
+         weatherIcon.src = `assets/weathericons/snow.png` ;
+       } else if (dayWeather === 'ts') {
+         weatherIcon.src = `assets/weathericons/ts.png` ; 
+       } else if (dayWeather === 'tsrain') {
+         weatherIcon.src = `assets/weathericons/tsrain.png` ;
+       } else {
+         weatherIcon.src = `assets/weathericons/oops.png`
+       }
+      liWeather.append(weatherIcon)     
       liDate.append(dayDate)
       liWeather.append(dayWeather)
       // dayCard.append(liDate, liWeather)
