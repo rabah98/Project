187a96016a6df9492f3d75e27d5289b0ab821aee
Alex <friedmanalexg@gmail.com>
2022-07-11 14:16:36 -0400

    messed with the fetch

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 
 //fetch function
 function fetchfunction() {
-   fetch('http://www.7timer.info/bin/astro.php?lon=113.17&lat=23.09&ac=0&lang=en&unit=metric&output=internal&tzshift=0')
+   fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civillight&output=json')
    .then(res => res.json())
-   .then(console.log)
+   .catch(errorCatch => {
+      console.log(errorCatch);
+   })
 }
 
 //render functions
