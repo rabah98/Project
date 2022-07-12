d190b5f634454f0ffe572540510945ef6d7cf89c
Alex <friedmanalexg@gmail.com>
2022-07-11 21:26:18 -0400

    added and styled the dog asset

--- pastey.js
+++ pastey.js
@@ -1,17 +1,25 @@
-if (dayWeather = 'clear') {
-    weatherIcon.src = `.assets/weathericons/clear.png` ;
-  } else if (dayWeather = 'pcloudy') {
-    weatherIcon.src = `.assets/weathericons/pcloudy.png` ;
-  } else if (dayWeather = 'cloudy') {
-    weatherIcon.src = `.assets/weathericons/cloudy.png` ;
-  } else if (dayWeather = 'rain' || 'lightrain') {
-    weatherIcon.src = `.assets/weathericons/rain.png` ;   
-  } else if (dayWeather = 'snow' || 'lightsnow') {
-    weatherIcon.src = `.assets/weathericons/snow.png` ;
-  } else if (dayWeather = 'ts') {
-    weatherIcon.src = `.assets/weathericons/ts.png` ; 
-  } else if (dayWeather = 'tsrain') {
-    weatherIcon.src = `.assets/weathericons/tsrain.png` ;
+if (dayWeather === 'clear') {
+    writeWeather.textContent = 'Clear';
+    weatherIcon.src = `assets/weathericons/clear.png` ;
+  } else if (dayWeather === 'pcloudy') {
+    writeWeather.textContent = 'Partly Cloudy';
+    weatherIcon.src = `assets/weathericons/pcloudy.png` ;
+  } else if (dayWeather === 'cloudy') {
+    writeWeather.textContent = 'Cloudy';
+    weatherIcon.src = `assets/weathericons/cloudy.png` ;
+  } else if (dayWeather === 'rain' || 'lightrain') {
+    writeWeather.textContent = 'Rain';
+    weatherIcon.src = `assets/weathericons/rain.png` ;   
+  } else if (dayWeather === 'snow' || 'lightsnow') {
+    writeWeather.textContent = 'Snow';
+    weatherIcon.src = `assets/weathericons/snow.png` ;
+  } else if (dayWeather === 'ts') {
+    writeWeather.textContent = 'Thunder Storm';
+    weatherIcon.src = `assets/weathericons/ts.png` ; 
+  } else if (dayWeather === 'tsrain') {
+    writeWeather.textContent = 'Thunder and Rain';
+    weatherIcon.src = `assets/weathericons/tsrain.png` ;
   } else {
-    weatherIcon.src = `.assets/weathericons/oops.png`
-  } 
+    writeWeather.textContent = "there was error:("
+    weatherIcon.src = `assets/weathericons/oops.png`
+  }
